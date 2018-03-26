30a02a61b1267b4758d64ca1e9c76bbce9560ab2
zanerammalUOIT <zane.rammal@uoit.net>
2018-03-26 10:44:56 -0400

    Lines starting
    This is Zane. Zach you need to pull this.

--- src/CodeReview/BuggyCode.java
+++ src/CodeReview/BuggyCode.java
@@ -9,123 +9,93 @@ public class BuggyCode {
 	
 	public static void main(final String[] args) {
 
+		System.out.println("Zane was here");
+		
 		System.out.println("BuggyCode Sample 001 for BC_IMPOSSIBLE_CAST");
-		// WRONG
 		try {
-			BuggyCode.bcImpossibleCastWRONG();
+			BuggyCode.bcImpossibleCastCORRECT();
 		} catch (final ClassCastException e) {
 			System.out.println("   - ERROR:" + e.getMessage());
 		}
-		// CORRECT
-		BuggyCode.bcImpossibleCastCORRECT();
-
+	
 		System.out.println("BuggyCode Sample 002 for BC_IMPOSSIBLE_DOWNCAST");
-		// WRONG
 		try {
-			BuggyCode.bcImpossibleDowncastWRONG();
+			BuggyCode.bcImpossibleDowncastCORRECT();
 		} catch (final ClassCastException e) {
 			System.out.println("   - ERROR:" + e.getMessage());
 		}
-		// CORRECT
-		BuggyCode.bcImpossibleDowncastCORRECT();
-
 		System.out.println("BuggyCode Sample 003 for BC_IMPOSSIBLE_INSTANCEOF");
-		// WRONG
-		BuggyCode.bcImpossibleInstanceOfWRONG();
-		// CORRECT
 		BuggyCode.bcImpossibleInstanceOfCORRECT();
-
 		System.out.println("BuggyCode Sample 004 for BC_IMPOSSIBLE_DOWNCAST_OF_TOARRAY");
-		// WRONG
+	
 		try {
-			BuggyCode.bcImpossibleDowncastOfArrayWRONG();
+			BuggyCode.bcImpossibleDowncastOfArrayCORRECT();
 		} catch (final ClassCastException e) {
 			System.out.println("   - ERROR:" + e.getMessage());
 		}
-		// CORRECT
-		BuggyCode.bcImpossibleDowncastOfArrayCORRECT();
+
 
 		System.out.println("BuggyCode Sample 005 for DMI_BIGDECIMAL_CONSTRUCTED_FROM_DOUBLE");
-		// WRONG
-		BuggyCode.dmiBigDecimalConstructedFromDoubleWRONG();
-		// CORRECT
+
 		BuggyCode.dmiBigDecimalConstructedFromDoubleCORRECT();
 
+		
+
 		System.out.println("BuggyCode Sample 006 for ES_COMPARING_STRINGS_WITH_EQ");
-		// WRONG
-		BuggyCode.esComparingStringsWithEqWRONG();
-		// CORRECT
 		BuggyCode.esComparingStringsWithEqCORRECT();
+		
 
 		System.out.println("BuggyCode Sample 007 for VA_FORMAT_STRING_ILLEGAL");
-		// WRONG
 		try {
-			BuggyCode.vaFormatStringIllegalWRONG();
+			BuggyCode.vaFormatStringIllegalCORRECT();
 		} catch (final UnknownFormatConversionException e) {
 			System.out.println("   - ERROR:" + e.getMessage());
 		}
-		// CORRECT
-		BuggyCode.vaFormatStringIllegalCORRECT();
+	
 
 		System.out.println("BuggyCode Sample 008 for RV_RETURN_VALUE_IGNORED");
-		// WRONG
-		BuggyCode.rvReturnValueIgnoredWRONG();
-		// CORRECT
 		BuggyCode.rvReturnValueIgnoredCORRECT();
 
+
 		System.out.println("BuggyCode Sample 009 for NP_ALWAYS_NULL");
-		// WRONG
 		try {
-			BuggyCode.npAlwaysNullWRONG();
+			BuggyCode.npAlwaysNullCORRECT();
 		} catch (final NullPointerException e) {
 			System.out.println("   - ERROR:" + e.getMessage());
 		}
 		// CORRECT
-		BuggyCode.npAlwaysNullCORRECT();
+
 
 		System.out.println("BuggyCode Sample 010 for QBA_QUESTIONABLE_BOOLEAN_ASSIGNMENT");
-		// WRONG
-		BuggyCode.qabQuestionableBooleanAssignmentWRONG();
 		// CORRECT
 		BuggyCode.qabQuestionableBooleanAssignmentCORRECT();
+	
 
 	}
 
-	private static void bcImpossibleCastWRONG() {
+	private static void bcImpossibleCastCORRECT() {
 		final Object doubleValue = Double.valueOf(1.0);
 		final Long value = (Long) doubleValue;
 		System.out.println("   - " + value);
 	}
 
-	private static void bcImpossibleCastCORRECT() {
-		final Object doubleValue = Double.valueOf(1.0);
-		final Double value = (Double) doubleValue;
-		System.out.println("   - " + value);
-	}
+	
 
-	private static void bcImpossibleDowncastWRONG() {
+	private static void bcImpossibleDowncastCORRECT() {
 		final Object exception = new RuntimeException("abc");
 		final SecurityException value = (SecurityException) exception;
 		System.out.println("   - " + value.getMessage());
 	}
 
-	private static void bcImpossibleDowncastCORRECT() {
-		final Object exception = new RuntimeException("abc");
-		final RuntimeException value = (RuntimeException) exception;
-		System.out.println("   - " + value.getMessage());
-	}
 
-	private static void bcImpossibleInstanceOfWRONG() {
+
+	private static void bcImpossibleInstanceOfCORRECT() {
 		final Object value = Double.valueOf(1.0);
 		System.out.println("   - " + (value instanceof Long));
 	}
 
-	private static void bcImpossibleInstanceOfCORRECT() {
-		final Object value = Double.valueOf(1.0);
-		System.out.println("   - " + (value instanceof Double));
-	}
 
-	private static void bcImpossibleDowncastOfArrayWRONG() {
+	private static void bcImpossibleDowncastOfArrayCORRECT() {
 		final Collection<String> stringVector = new ArrayList<String>();
 		stringVector.add("abc");
 		stringVector.add("xyz");
@@ -133,25 +103,14 @@ public class BuggyCode {
 		System.out.println("   - " + stringArray.length);
 	}
 
-	private static void bcImpossibleDowncastOfArrayCORRECT() {
-		final Collection<String> stringVector = new ArrayList<String>();
-		stringVector.add("abc");
-		stringVector.add("xyz");
-		final String[] stringArray = stringVector.toArray(new String[stringVector.size()]);
-		System.out.println("   - " + stringArray.length);
-	}
 
-	private static void dmiBigDecimalConstructedFromDoubleWRONG() {
+	private static void dmiBigDecimalConstructedFromDoubleCORRECT() {
 		final BigDecimal bigDecimal = new BigDecimal(3.1);
 		System.out.println("   - " + bigDecimal.toString());
 	}
 
-	private static void dmiBigDecimalConstructedFromDoubleCORRECT() {
-		final BigDecimal bigDecimal = new BigDecimal("3.1");
-		System.out.println("   - " + bigDecimal.toString());
-	}
 
-	private static void esComparingStringsWithEqWRONG() {
+	private static void esComparingStringsWithEqCORRECT() {
 		final StringBuilder sb1 = new StringBuilder("1234");
 		final StringBuilder sb2 = new StringBuilder("1234");
 		final String string1 = sb1.toString();
@@ -159,35 +118,22 @@ public class BuggyCode {
 		System.out.println("   - " + (string1 == string2));
 	}
 
-	private static void esComparingStringsWithEqCORRECT() {
-		final StringBuilder sb1 = new StringBuilder("1234");
-		final StringBuilder sb2 = new StringBuilder("1234");
-		final String string1 = sb1.toString();
-		final String string2 = sb2.toString();
-		System.out.println("   - " + string1.equals(string2));
-	}
 
-	private static void vaFormatStringIllegalWRONG() {
+
+	private static void vaFormatStringIllegalCORRECT() {
 		System.out.println(String.format("   - %>s  %s", "10", "9"));
 	}
 
-	private static void vaFormatStringIllegalCORRECT() {
-		System.out.println(String.format("   - %s > %s", "10", "9"));
-	}
 
-	private static void rvReturnValueIgnoredWRONG() {
+	private static void rvReturnValueIgnoredCORRECT() {
 		final BigDecimal bigDecimal = BigDecimal.ONE;
 		bigDecimal.add(BigDecimal.ONE);
 		System.out.println(String.format("   - " + bigDecimal));
 	}
 
-	private static void rvReturnValueIgnoredCORRECT() {
-		final BigDecimal bigDecimal = BigDecimal.ONE;
-		final BigDecimal newValue = bigDecimal.add(BigDecimal.ONE);
-		System.out.println(String.format("   - " + newValue));
-	}
 
-	private static void npAlwaysNullWRONG() {
+
+	private static void npAlwaysNullCORRECT() {
 		final String value = null;
 		if (null != value & value.length() > 2) {
 			System.out.println(String.format("   - " + value));
@@ -196,16 +142,9 @@ public class BuggyCode {
 		}
 	}
 
-	private static void npAlwaysNullCORRECT() {
-		final String value = null;
-		if (null != value && value.length() > 2) {
-			System.out.println(String.format("   - " + value));
-		} else {
-			System.out.println(String.format("   - value is invalid"));
-		}
-	}
 
-	private static void qabQuestionableBooleanAssignmentWRONG() {
+
+	private static void qabQuestionableBooleanAssignmentCORRECT() {
 		boolean value = false;
 		if (value = true) {
 			System.out.println(String.format("   - value is true"));
@@ -214,13 +153,5 @@ public class BuggyCode {
 		}
 	}
 
-	private static void qabQuestionableBooleanAssignmentCORRECT() {
-		final boolean value = false;
-		if (value == true) {
-			System.out.println(String.format("   - value is true"));
-		} else {
-			System.out.println(String.format("   - value is false"));
-		}
-	}
-
+		
 }
